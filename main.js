2fff02a21aa8229c64e2b3267a8c1e2fbbb52df0
HaoRyou <158393369+HaoRyou@users.noreply.github.com>
2025-07-07 11:28:48 -0400

    Editing: Editing style.css and index.html style.css: enable display: flex, flex:1, and hover. Html: Add function when Button is click, pop up to ask for input and create a grid using that input.

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
  const mainstorage = document.getElementById("container");
 
 
-for(let i=0;i<16;i++){
+ function creategrid(num){
+    for(let i=0;i<num;i++){
     const firstlayer = document.createElement('div');
     firstlayer.classList.add("Column");
-    for(let z=1;z<=16;z++){
-        const secondlayer = document.createElement('div');
-        secondlayer.classList.add("Row");
-        secondlayer.textContent =`${z}`;
-        secondlayer.style.border = "1px Solid black";
-        secondlayer.style.margin = "2px";
-        firstlayer.appendChild(secondlayer);
-    }
+        for(let z=1;z<=num;z++){
+            const secondlayer = document.createElement('div');
+            secondlayer.classList.add("Row");
+            secondlayer.textContent =`${z}`;
+            secondlayer.style.border = "1px Solid black";
+            secondlayer.style.margin = "2px";
+            firstlayer.appendChild(secondlayer);
+        }
     mainstorage.appendChild(firstlayer);
-}
+    }
+ }
+
+ const inputing = document.getElementById("input");
+
+
+
+ inputing.addEventListener("click", () => {
+    let num = Number(prompt("Enter Your number for Grid"));
+    inputing.style.visibility = "hidden";
+    creategrid(num);
+ })
+
